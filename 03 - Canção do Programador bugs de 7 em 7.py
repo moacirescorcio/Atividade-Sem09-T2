1fc650d904d4b68d12bcefd693223581d2c888d8
moacirescorcio <moacirescorcio@gmail.com>
2021-10-29 17:22:59 -0300

    Update 03 - Canção do Programador bugs de 7 em 7.py

--- 03 - Canção do Programador bugs de 7 em 7.py
+++ 03 - Canção do Programador bugs de 7 em 7.py
@@ -1,5 +1,5 @@
 for c in range(99, 251, 7):
-    print(f'{c} bugs no software, pegue um deles e conserte...')
+    print(f'{c} bugs no software, pegue sete deles e conserte...')
     print('Tecle "Ctrl+F5" ')
 
 print('Vamos fazer mais um café!')
